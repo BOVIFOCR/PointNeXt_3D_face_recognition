cf6a11c9dc0ebca09a2098660c0b9f70e335ccac
Bernardo Biesseck <biesseck@gmail.com>
2023-05-08 18:43:36 -0300

    Rename 'run_verification_test.py' to 'run_verification_test_one_dataset.py'

--- examples/classification/train.py
+++ examples/classification/train.py
@@ -14,7 +14,7 @@ from openpoints.scheduler import build_scheduler_from_cfg
 from openpoints.models import build_model_from_cfg
 from openpoints.models.layers import furthest_point_sample, fps
 
-from examples.face_verification.run_verification_test import VerificationTester
+from examples.face_verification.run_verification_test_one_dataset import VerificationTester
 
 
 def get_features_by_keys(input_features_dim, data):
