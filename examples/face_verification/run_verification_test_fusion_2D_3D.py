0cc6c26455c5edf731b16da245375e6f5c963c01
Bernardo Biesseck <biesseck@gmail.com>
2023-05-31 17:04:09 -0300

    examples/face_verification/run_verification_test_fusion_2D_3D.py: implement 3 distances fusion methods: 'mean', 'min' and 'max'

--- examples/face_verification/run_verification_test_fusion_2D_3D.py
+++ examples/face_verification/run_verification_test_fusion_2D_3D.py
@@ -23,31 +23,47 @@ class LateFusionVerificationTester(VerificationTester):
         super().__init__()
 
 
-    def fuse_scores(self, distances1, distances2):
+    def fuse_scores(self, distances1, distances2, method='mean'):
         assert distances1.shape[0] == distances2.shape[0]
         distances1 /= np.max(distances1)
         distances2 /= np.max(distances2)
-        final_distances = (distances1 + distances2) / 2.0
+
+        if method == 'mean':
+            final_distances = (distances1 + distances2) / 2.0
+        elif method == 'min':
+            final_distances = np.minimum(distances1, distances2)
+        elif method == 'max':
+            final_distances = np.maximum(distances1, distances2)
+
         return final_distances
 
 
-    def do_fusion_verification_test(self, model, dataset='LFW', num_points=2048, distances_pairs_2d=np.array([]), batch_size=32, verbose=True):
+    def do_fusion_verification_test(self, model, dataset='LFW', num_points=2048, distances_pairs_2d=np.array([]), batch_size=32, fusion_methods=['mean'], verbose=True):
         model.eval()
-
-        # train_distances_2d = distances_pairs_2d['train']
-        # test_distances_2d = distances_pairs_2d['test']
 
         folds_pair_cache, folds_pair_labels, folds_indexes = self.load_organize_and_subsample_pointclouds(dataset, num_points, verbose=verbose)
 
         folds_pair_distances = self.compute_set_distances(model, folds_pair_cache, batch_size, verbose=verbose)
         folds_pair_distances = folds_pair_distances.cpu().detach().numpy()
 
-        distances_fused = self.fuse_scores(folds_pair_distances, distances_pairs_2d)
+        distances_fused = {}
+        results_fused = {}
+        for fm in fusion_methods:
+            # distances_fused = self.fuse_scores(folds_pair_distances, distances_pairs_2d)
+            distances_fused[fm] = self.fuse_scores(folds_pair_distances, distances_pairs_2d, method=fm)
 
-        tpr, fpr, accuracy, tar_mean, tar_std, far_mean = self.do_k_fold_test(distances_fused, folds_pair_labels, folds_indexes, verbose=verbose)
-        acc_mean, acc_std = np.mean(accuracy), np.std(accuracy)
+            tpr, fpr, accuracy, tar_mean, tar_std, far_mean = self.do_k_fold_test(distances_fused[fm], folds_pair_labels, folds_indexes, verbose=verbose)
+            acc_mean, acc_std = np.mean(accuracy), np.std(accuracy)
 
-        return acc_mean, acc_std, tar_mean, tar_std, far_mean
+            results_fused[fm] = {}
+            results_fused[fm]['acc_mean'] = acc_mean
+            results_fused[fm]['acc_std'] = acc_std
+            results_fused[fm]['tar_mean'] = tar_mean
+            results_fused[fm]['tar_std'] = tar_std
+            results_fused[fm]['far_mean'] = far_mean
+
+        # return acc_mean, acc_std, tar_mean, tar_std, far_mean
+        return results_fused
 
 
 
@@ -77,8 +93,21 @@ if __name__ == "__main__":
     # print('distances_pairs_2d:', distances_pairs_2d.device)
     # sys.exit(0)
 
-    # Do fused verification test
-    acc_mean, acc_std, tar, tar_std, far = late_fusion_verif_tester.do_fusion_verification_test(model, args.dataset, args.num_points, distances_pairs_2d, args.batch, verbose=True)
+    # fusion_methods = ['mean']
+    # fusion_methods = ['mean', 'min']
+    fusion_methods = ['mean', 'min', 'max']
 
-    print('\nFinal - dataset: %s  -  acc_mean: %.6f ± %.6f  -  tar: %.6f ± %.6f    far: %.6f)' % (args.dataset, acc_mean, acc_std, tar, tar_std, far))
+    # Do fused verification test
+    # acc_mean, acc_std, tar, tar_std, far = late_fusion_verif_tester.do_fusion_verification_test(model, args.dataset, args.num_points, distances_pairs_2d, args.batch, verbose=True)
+    results_fused = late_fusion_verif_tester.do_fusion_verification_test(model, args.dataset, args.num_points, distances_pairs_2d, args.batch, fusion_methods, verbose=True)
+    print()
+
+    for fm in fusion_methods:
+        acc_mean = results_fused[fm]['acc_mean']
+        acc_std = results_fused[fm]['acc_std']
+        tar_mean = results_fused[fm]['tar_mean']
+        tar_std = results_fused[fm]['tar_std']
+        far_mean = results_fused[fm]['far_mean']
+        print('Final - dataset: %s  -  fusion_method: %s  -  acc_mean: %.6f ± %.6f  -  tar_mean: %.6f ± %.6f    far_mean: %.6f)' % (args.dataset, fm, acc_mean, acc_std, tar_mean, tar_std, far_mean))
+
     print('Finished!')
