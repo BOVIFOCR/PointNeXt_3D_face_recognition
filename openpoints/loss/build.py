dcfaa0aa9bcb5c46f731ae6970419cf828c93ffb
Bernardo Biesseck <biesseck@gmail.com>
2023-08-18 16:00:47 -0300

    openpoints/loss/build.py: add class 'CombinedMarginLoss'; cfgs/ms1mv3_3d_hrn_CombinedMarginLoss: add config files for CombinedMarginLoss

--- openpoints/loss/build.py
+++ openpoints/loss/build.py
@@ -2,12 +2,77 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import CrossEntropyLoss, BCEWithLogitsLoss
+import math
 from openpoints.utils import registry
 
 LOSS = registry.Registry('loss')
 LOSS.register_module(name='CrossEntropy', module=CrossEntropyLoss)
 LOSS.register_module(name='CrossEntropyLoss', module=CrossEntropyLoss)
 LOSS.register_module(name='BCEWithLogitsLoss', module=BCEWithLogitsLoss)
+
+
+@LOSS.register_module()
+class CombinedMarginLoss(torch.nn.Module):
+    def __init__(self, 
+                 s, 
+                 m1,
+                 m2,
+                 m3,
+                 interclass_filtering_threshold=0):
+        super().__init__()
+        self.s = s
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+        self.interclass_filtering_threshold = interclass_filtering_threshold
+        
+        # For ArcFace
+        self.cos_m = math.cos(self.m2)
+        self.sin_m = math.sin(self.m2)
+        self.theta = math.cos(math.pi - self.m2)
+        self.sinmm = math.sin(math.pi - self.m2) * self.m2
+        self.easy_margin = False
+
+    def forward(self, logits, labels):
+        logits = logits.clone()
+        index_positive = torch.where(labels != -1)[0]
+
+        if self.interclass_filtering_threshold > 0:
+            with torch.no_grad():
+                dirty = logits > self.interclass_filtering_threshold
+                dirty = dirty.float()
+                mask = torch.ones([index_positive.size(0), logits.size(1)], device=logits.device)
+                mask.scatter_(1, labels[index_positive], 0)
+                dirty[index_positive] *= mask
+                tensor_mul = 1 - dirty    
+            logits = tensor_mul * logits
+
+        target_logit = logits[index_positive, labels[index_positive].view(-1)]
+
+        if self.m1 == 1.0 and self.m3 == 0.0:
+            with torch.no_grad():
+                target_logit.arccos_()
+                logits.arccos_()
+                final_target_logit = target_logit + self.m2
+                logits[index_positive, labels[index_positive].view(-1)] = final_target_logit
+                logits.cos_()
+            logits = logits * self.s        
+
+        elif self.m3 > 0:
+            final_target_logit = target_logit - self.m3
+            logits[index_positive, labels[index_positive].view(-1)] = final_target_logit
+            logits = logits * self.s
+        else:
+            raise
+
+        # return logits
+        gt = labels.contiguous().view(-1)
+        loss = torch.nn.CrossEntropyLoss()(logits, gt)
+        return loss
+        # if self.return_valid:
+        #     return loss, logits, gt
+        # else:
+        #     return loss
 
 
 @LOSS.register_module()
