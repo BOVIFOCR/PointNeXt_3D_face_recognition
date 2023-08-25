09f9363837029181d944df54bd4a29bb08469f9d
Bernardo Biesseck <biesseck@gmail.com>
2023-08-25 15:41:04 -0300

    Add 'openpoints/dataset/ms1mv3_3d_hrn/ms1mv3_3d_hrn_1000subj_loader.py'

--- openpoints/dataset/ms1mv3_3d_hrn/__init__.py
+++ openpoints/dataset/ms1mv3_3d_hrn/__init__.py
@@ -1,6 +1,6 @@
 from .ms1mv3_3d_hrn_loader import MS1MV3_3D_HRN
 from .ms1mv3_3d_hrn_22subj_loader import MS1MV3_3D_HRN_22subj
-# from .ms1mv3_3d_hrn_1000subj_loader import MS1MV3_3D_HRN_1000subj
+from .ms1mv3_3d_hrn_1000subj_loader import MS1MV3_3D_HRN_1000subj
 # from .ms1mv3_3d_hrn_2000subj_loader import MS1MV3_3D_2000subj
 # from .ms1mv3_3d_hrn_5000subj_loader import MS1MV3_3D_5000subj
 # from .ms1mv3_3d_hrn_10000subj_loader import MS1MV3_3D_10000subj
